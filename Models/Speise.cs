1f9471b2f8d1dcbde5c1a9ea7bb1bd1a84f35775
Leon Rothauer <90833349+LeonRothauer@users.noreply.github.com>
2024-03-07 07:59:01 +0100

    Add files via upload

---
+++ Models/Speise.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Speisekarte.Models
+{
+    public class Speise
+    {
+        public int Id { get; set; }
+        public string Titel { get; set; }
+        public string? Notizen { get; set; }
+        public int Sterne { get; set; }
+
+        // 1:n Beziehung
+        [JsonIgnore]
+        public List<Zutat> Zutaten { get; set; } = new List<Zutat>();
+
+    }
+}
